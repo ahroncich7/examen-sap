89bf2048b41f92289b4531770f0fba463a0231fb
Alejandro Hroncich <alehroncich@gmail.com>
2023-06-21 17:13:23 +0000

    Logra conectar los datos al popup

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,15 +1,50 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Fragment) {
         "use strict";
 
         return Controller.extend("ejercicio.examen.controller.Master", {
             onInit: function () {
 
+            },
+
+            onClicked: function(oEvent){
+                var oView = this.getView();
+                var sPath = oEvent.getSource().getBindingContext().getPath();
+
+                if (!this.byId("openDialog")) {
+                    Fragment.load(
+                        {
+                            id: oView.getId(),
+                            name: "ejercicio.examen.view.fragments.ProductDetail",
+                            controller: this
+                        }).then(
+                            function (oDialog) {
+                                oDialog.bindElement({
+                                    path: sPath
+                                  });
+                                oView.addDependent(oDialog);
+                                oDialog.open();
+                            }
+                        )
+                } else {
+                    this.byId("openDialog").bindElement({
+                        path: sPath
+                      });
+                    this.byId("openDialog").open()
+                }
+
+            },
+
+            onClose: function(){
+                this.byId("openDialog").close()
             }
+
+            
         });
     });
