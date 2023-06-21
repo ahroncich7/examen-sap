60e8a81866e1d158f9e3c081ef08e6daeabc639d
Alejandro Hroncich <alehroncich@gmail.com>
2023-06-21 17:21:44 +0000

    Logra routing en el manifest

---
+++ webapp/controller/DetailOrders.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, Fragment) {
+        "use strict";
+
+        return Controller.extend("ejercicio.examen.controller.DetailOrders", {
+            onInit: function () {
+
+            }
+
+            
+        });
+    });
