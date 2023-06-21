2d62b215c951f74d082c69563408625477cc23b1
Alejandro Hroncich <alehroncich@gmail.com>
2023-06-21 15:55:28 +0000

    Initial Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ejercicio/examen/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ejercicio/examen/test/unit/controller/Master.controller"
+], function () {
+	"use strict";
+});
